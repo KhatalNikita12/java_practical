4e7f14cfcbad625530ce4f16853a94a42eda4d6f
KhatalNikita12 <Khatalnikita2003@gmail.com>
2024-06-11 13:59:47 +0530

    java practical

--- file3.java
+++ file3.java
@@ -1,25 +1,28 @@
-import java.io.*;
-public class file3 {
-    public static void main(String[] args) 
-    {
-        String fname=args[0];
-        int l=0,c=0,w=0;
-        try{
-            FileReader f=new FileReader(fname);
-            BufferedReader b=new BufferedReader(f);
-            String line;
-            while((line=b.readLine())!=null){
-                l++;
-                String word[]=line.split("\\s+");
-                w+=word.length;
-                c+=line.length();
-            }
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
-        System.out.println("total number of line "+l);
-        System.out.println("total number of character "+c);
-        System.out.println("total number of word "+w);
+import java.sql.*;
+
+public class file3  {
+public static void main(String args[]){
+
+    try{
+
+        Connection  con=DriverManager.getConnection("jdbc:postgresql://localhost/postgres","postgres","123");
+        Statement stmt=con.createStatement();
+        DatabaseMetaData dbmd=con.getMetaData();
+        Class.forName("org.postgresql.Driver");
+        ResultSet rs=stmt.executeQuery("select * from student");
+        ResultSetMetaData rsmd=rs.getMetaData();
+        int noofcol=rsmd.getColumnCount();
+        System.out.println("number of column"+noofcol);
+for(int i=1;i<=noofcol;i++){
+    System.out.println("column no:"+i);
+    System.out.println("column name:"+rsmd.getColumnName(i));
+    System.out.println("Column type:"+rsmd.getColumnTypeName(i));
+    System.out.println("column dispaly type:"+rsmd.getColumnDisplaySize(i));
+}
+        con.close();
+    }
+    catch (Exception e){
+        System.out.println(e);
     }
 }
+    }
